c69242544de61a36ead103aebfde62a3daea2a84
Arthur K <klezovich@gmail.com>
2019-05-21 22:50:47 +0200

    Add typeform api support

---
+++ src/test/java/com/klezovich/demo/components/typeform/TypeformServiceTest.java
@@ -0,0 +1,26 @@
+package com.klezovich.demo.components.typeform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TypeformServiceTest {
+
+    @Autowired
+    TypeformService service;
+
+    @Test
+    public void correctEmailIsRetrieved(){
+
+        var result = service.getMyEmail();
+        assertEquals( "klezovich@gmail.com", result  );
+    }
+
+}
